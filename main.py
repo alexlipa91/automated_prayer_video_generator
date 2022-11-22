147b415f6508cf3f3ee378bc7591eb8bffb8c108
alessandroliparoti <alessandro.lipa@gmail.com>
2022-11-22 16:21:02 +0700

    fix runner

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
+import sys
 
 import uploader
 from audio import AudioDownloader
 from video import VideoDownloader, VideoComposer
 
 if __name__ == '__main__':
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "prayers-channel-369405-3b2a01d84758.json"
-    year = 2022
-    month = 11
-    day = 20
+    param_tokens = sys.argv[1].split("-")
+    year = param_tokens[0]
+    month = param_tokens[1]
+    day = param_tokens[2]
 
     ad = AudioDownloader(year=year, month=month, day=day)
     ad.run()
