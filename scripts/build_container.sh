1ca818529230fc2a2188ebc892b406fc056c0197
Alessandro Liparoti <alessandroliparoti@live.it>
2024-10-15 10:34:04 +0200

    refactor build and push scripts

---
+++ scripts/build_container.sh
@@ -0,0 +1,2 @@
+IMAGE_NAME=europe-west4-docker.pkg.dev/prayers-channel-369405/images/prayer-channel-video-builder
+docker buildx build --platform linux/amd64 -t $IMAGE_NAME:$(git rev-parse HEAD) -t $IMAGE_NAME:latest .
--- scripts/push_container.sh
+++ scripts/push_container.sh
@@ -1,1 +1,4 @@
-docker push europe-west4-docker.pkg.dev/prayers-channel-369405/images/prayer-channel-video-builder:latest
+IMAGE_NAME=europe-west4-docker.pkg.dev/prayers-channel-369405/images/prayer-channel-video-builder
+
+docker buildx build --platform linux/amd64 -t $IMAGE_NAME:$(git rev-parse HEAD) -t $IMAGE_NAME:latest .
+docker push $IMAGE_NAME:$(git rev-parse HEAD) --all-tags
